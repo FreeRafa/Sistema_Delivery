3dd8d945efcf805788aec9c443ecdb36a7853ea0
Rafael Velloso <rafavelloso2@gmail.com>
2026-08-30 19:37:15 +0100

    Adiciona SistemaDeliveryContext com DbSets e configuração de conexão

---
+++ SistemaDelivery/Infrastructure/Data/SistemaDeliveryContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaDelivery.Infrastructure.Data
+{
+    public class SistemaDeliveryContext : DbContext
+    {
+        public SistemaDeliveryContext(DbContextOptions<SistemaDeliveryContext> options) : base (options)
+        {
+        }
+
+        public DbSet<Cliente> Clientes => Set<Clientes>();
+        public DbSet<Restaurante> Restaurantes => Set<Restaurantes>();
+        public DbSet<Prato> Pratos => Set<Pratos>();
+        public DbSet<Pedido> Pedidos => Set<Pedido>();
+        public DbSet<ItemPedido> ItemsPedidos => Set<ItemPedido>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(SistemaDeliveryContext).Assembly);
+        }
+    }
+}
---
+++ SistemaDelivery/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SistemaDelivery.Infrastructure.Data;
+
+
+IConfiguration config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+string connectionString = config.GetConnectionString("SistemaDelivery")!;
+
+var services = new ServiceCollection();
+
+services.AddDbContext<SistemaDeliveryContext>(options =>
+    options.UseSqlServer(connectionString));
+
+using var serviceProvider = services.BuildServiceProvider();
+using var scope = serviceProvider.CreateScope();
+
+
+
+
+
+
+
+
+//string connectionString = config.GetConnectionString("SistemaDelivery")!;
+
+//using (SqlConnection conn = new SqlConnection(connectionString))
+//{
+//    try
+//    {
+//        conn.Open();
+//        Console.WriteLine("Ligação feita com sucesso!");
+//    }
+//    catch (Exception ex)
+//    {
+//        Console.WriteLine("Erro ao ligar: " + ex.Message);
+//    }
+//}
+
