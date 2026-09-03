8224b330b31b10e729f2164a05df79adb2a31a38
Rafael Velloso <rafavelloso2@gmail.com>
2026-09-03 22:32:54 +0100

    Implementado ID no program

--- SistemaDelivery/Program.cs
+++ SistemaDelivery/Program.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SistemaDelivery.Infrastructure.Data;
+using SistemaDelivery.Infrastructure.Repositorio;
+using SistemaDelivery.Modelo.Entidades;
+using SistemaDelivery.Modelo.Interfaces;
+using SistemaDelivery.Servico;
 
 
 IConfiguration config = new ConfigurationBuilder()
@@ -17,6 +20,22 @@ var services = new ServiceCollection();
 
 services.AddDbContext<SistemaDeliveryContext>(options =>
     options.UseSqlServer(connectionString));
+
+services.AddScoped<IClienteRepositorio, ClienteRepositorio>();
+services.AddScoped<ClienteServico>();
+
+services.AddScoped<IPedidoRepositorio, PedidoRepositorio>();
+services.AddScoped<PedidoServico>();
+
+services.AddScoped<IItemPedidoRepositorio, ItemPedidoRepositorio>();
+services.AddScoped<ItemPedido>();
+
+services.AddScoped<IPratoRepositorio, PratoRepositorio>();
+services.AddScoped<PratoServico>();
+
+services.AddScoped<IRestauranteRepositorio, RestauranteRepositorio>();
+services.AddScoped<RestauranteServico>();
+
 
 using var serviceProvider = services.BuildServiceProvider();
 using var scope = serviceProvider.CreateScope();
